95c23a3f5709fb353b5055a83803e23baea2b4eb
Chitra Gadwal <cgadwa1@umbc.edu>
2013-11-22 23:23:59 -0500

    wrote getflights method in sqlcommands

--- DB_Project/src/db_proj/SqlCommands.java
+++ DB_Project/src/db_proj/SqlCommands.java
@@ -1,4 +1,3 @@
-package db_proj;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,9 +9,9 @@ public class SqlCommands {
 	static final String USER = "cgadwa1"; //Please add your User ID for your Oracle Account
 	static final String PASS = "babylips";  //Please add your Oracle Password
 
-	public ArrayList<String> getListOfUsers(){
+	public static String signIn(String username, String password){
 		
-		ArrayList<String> usersList = new ArrayList<String>();
+		String uName = "";
 		Connection conn = null;
 		Statement stmt = null;
 		try
@@ -22,12 +21,16 @@ public class SqlCommands {
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			System.out.println("Executing...");
 			stmt = conn.createStatement();
-			ResultSet dbUsers=  stmt.executeQuery("SELECT USERNAME, PASSWORD from USERS");
-			usersList = new ArrayList<String>();
-			while(dbUsers.next()){
-				usersList.add(dbUsers.getString("username"));
-		    }
-			System.out.println("Action done!");
+			String sql = "select username from users where username = '" + username + "'";
+			System.out.println(sql);
+			ResultSet dbUsers = stmt.executeQuery(sql);
+			
+				dbUsers.next();
+				System.out.println("boo");
+				uName = dbUsers.getString("username");
+				
+
+			System.out.println(uName);
 		}catch(SQLException se){
 	   		se.printStackTrace();
 	   	}catch(Exception e){
@@ -46,24 +49,120 @@ public class SqlCommands {
 		   	}
 	   	}
 		
-		return usersList;
+		return uName;
 	}
 	
-	public ArrayList getListOfPassengers(){
+	public static ArrayList<String> getListOfPassengers(){
+		String fName = "";
+		String lName = "";
+		int ssn;
+		ArrayList<String> passList = new ArrayList<String>();
+		Connection conn = null;
+		Statement stmt = null;
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Executing...");
+			stmt = conn.createStatement();
+			String sql = "select firstName, lastName, ssn from passenger";
+			System.out.println(sql);
+			ResultSet dbUsers = stmt.executeQuery(sql);
+			
+				while(dbUsers.next()){
+				fName = dbUsers.getString("firstName");
+				lName = dbUsers.getString("lastName");
+				ssn = dbUsers.getInt("ssn");
+				String s = fName +" "+ lName +" "+ ssn;
+				passList.add(s);
+				
+				}
+
+			
+		}catch(SQLException se){
+	   		se.printStackTrace();
+	   	}catch(Exception e){
+	   		e.printStackTrace();
+	   	}finally{
+	   		try{
+	   			if(stmt!=null)
+	   				stmt.close();
+		   		}catch(SQLException se2){
+		   	}
+		   	try{
+		   		if(conn!=null)
+		   			conn.close();
+		   	}catch(SQLException se){
+		   		se.printStackTrace();
+		   	}
+	   	}
 		
-		
-		return null;
+		return passList;
 		
 		
 	}
 	
-	public ArrayList getListOfFlights(){
+	public static ArrayList<String> getListOfFlights(){
 		
+		int flightNumb;
 		
-		return null;
+		ArrayList<String> flightList = new ArrayList<String>();
+		Connection conn = null;
+		Statement stmt = null;
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Executing...");
+			stmt = conn.createStatement();
+			String sql = "select flightNum from flight";
+			System.out.println(sql);
+			ResultSet dbUsers = stmt.executeQuery(sql);
+			
+				while(dbUsers.next()){
+				flightNumb = dbUsers.getInt("flightnum");
+				String s = "" + flightNumb;
+				flightList.add(s);
+				
+				}
+
+			
+		}catch(SQLException se){
+	   		se.printStackTrace();
+	   	}catch(Exception e){
+	   		e.printStackTrace();
+	   	}finally{
+	   		try{
+	   			if(stmt!=null)
+	   				stmt.close();
+		   		}catch(SQLException se2){
+		   	}
+		   	try{
+		   		if(conn!=null)
+		   			conn.close();
+		   	}catch(SQLException se){
+		   		se.printStackTrace();
+		   	}
+	   	}
 		
+		return flightList;
+				
 		
 	}
 	
 	
+
+
+	public static void main(String[] args){
+	
+		ArrayList<String> list = getListOfFlights();
+			for(String s : list){
+				System.out.println(s);
+			}
+	
+	}
+
+
 }
